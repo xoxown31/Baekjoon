6ff7ddfad22d25c790ad28be87fde2925993a8f6
xoxown <73877449+xoxown31@users.noreply.github.com>
2025-05-27 17:23:13 +0900

    [Platinum V] Title: 배열에서 이동, Time: 104 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Platinum/1981. 배열에서 이동/배열에서 이동.cc
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.cc
@@ -1,42 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
-#include <climits>
 
 using namespace std;
+
+int MIN = -1;
+int MAX = 201;
 
 int n;
 vector<vector<int>> l;
 vector<pair<int, int>> dij = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-bool isin(int i, int j) {
-    return 0 <= i && i < n && 0 <= j && j < n;
+bool isin(int i, int j);
+
+bool bfs(int x, int m) {
+    if (!(m <= l[0][0] && l[0][0] <= m + x)) return false;;
+
+    vector<vector<bool>> visited(n, vector<bool>(n, false));
+    queue<pair<int, int>> q;
+    visited[0][0] = true;
+    q.push({0, 0});
+
+    while (!q.empty()) {
+        auto [ui, uj] = q.front(); q.pop();
+        if (ui == n - 1 && uj == n - 1) return true;
+
+        for (auto [di, dj] : dij) {
+            int vi = ui + di;
+            int vj = uj + dj;
+
+            if (!isin(vi, vj) || visited[vi][vj]) continue;
+            if (m <= l[vi][vj] && l[vi][vj] <= m + x) {
+                visited[vi][vj] = true;
+                q.push({vi, vj});
+            }
+        }
+    }
+    return false;
 }
 
-bool bfs(int x, int MIN, int MAX) {
-    for (int m = MIN; m <= MAX - x; ++m) {
-        if (!(m <= l[0][0] && l[0][0] <= m + x)) continue;
-
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        queue<pair<int, int>> q;
-        visited[0][0] = true;
-        q.push({0, 0});
-
-        while (!q.empty()) {
-            auto [ui, uj] = q.front(); q.pop();
-            if (ui == n - 1 && uj == n - 1) return true;
-
-            for (auto [di, dj] : dij) {
-                int vi = ui + di;
-                int vj = uj + dj;
-
-                if (!isin(vi, vj) || visited[vi][vj]) continue;
-                if (m <= l[vi][vj] && l[vi][vj] <= m + x) {
-                    visited[vi][vj] = true;
-                    q.push({vi, vj});
-                }
-            }
+bool f(int x) {
+    for(int m = MIN; m < MAX - x + 1; m++) {
+        if(bfs(x, m)) {
+            return true;
         }
     }
     return false;
@@ -49,9 +55,6 @@ int main() {
     cin >> n;
     l.resize(n, vector<int>(n));
 
-    int MIN = INT_MAX;
-    int MAX = INT_MIN;
-
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cin >> l[i][j];
@@ -63,7 +66,7 @@ int main() {
     int start = 0, end = MAX - MIN;
     while (end > start) {
         int mid = (start + end) / 2;
-        if (bfs(mid, MIN, MAX)) {
+        if (f(mid)) {
             end = mid;
         } else {
             start = mid + 1;
@@ -72,4 +75,8 @@ int main() {
 
     cout << end << '\n';
     return 0;
+}
+
+bool isin(int i, int j) {
+    return 0 <= i && i < n && 0 <= j && j < n;
 }
