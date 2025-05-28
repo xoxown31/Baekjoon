89ac951ef791fbaff0556e47da334490d59da711
xoxown <73877449+xoxown31@users.noreply.github.com>
2025-05-28 15:05:13 +0900

    [Platinum V] Title: 배열에서 이동, Time: 84 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Platinum/1981. 배열에서 이동/배열에서 이동.cc
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.cc
@@ -1,95 +1,82 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
-int MAX = 0;
-int MIN = 200;
 const int MAXSIZE = 100;
 
+int MAX = 0;
+int MIN = 200;
+
 int n;
-int l[MAXSIZE][MAXSIZE];
+int l[MAXSIZE+1][MAXSIZE+1];
 
 int dij[4][2] = {
-    {1, 0},
-    {-1, 0},
-    {0, -1},
-    {0, 1}
+    {0,1},
+    {0,-1},
+    {1,0},
+    {-1,0}
 };
 
-bool isin(int i, int j);
+bool f(int mid);
+bool bfs(int mid, int x);
 
-void input();
-void solve();
-
-bool f(int x);
-bool bfs(int x, int m);
-
-bool isin(int i, int j){
-    return 0 <= i && i < n && 0 <= j && j < n;
-}
-
-void input() {
+int main() {
     cin >> n;
 
     for(int i=0; i<n; i++){
-        for(int j = 0; j < n; j++){
+        for(int j=0; j<n; j++){
             cin >> l[i][j];
-            MAX = max(l[i][j], MAX);
-            MIN = min(l[i][j], MIN);
+            MAX = max(MAX, l[i][j]);
+            MIN = min(MIN, l[i][j]);
         }
     }
-}
 
-void solve(){
     int start = 0, end = MAX - MIN;
     while(end > start){
-        int mid = (start + end) / 2;
+        int mid = (start+end) / 2;
         if(f(mid)) end = mid;
-        else start = mid + 1;
+        else start = mid+1;
     }
 
-    cout << end << '\n';
-    return;
+    cout << end;
+
+    return 0;
 }
 
-bool f(int x){
-    for(int m = MIN; m < MAX - x + 1; m++){
-        if(bfs(x, m)) return true;
+bool f(int mid){
+    for(int x = MIN; mid+x <= MAX; x++){
+        if(bfs(mid, x)) return true;
     }
     return false;
 }
 
-bool bfs(int x, int m){
-    if(!(m <= l[0][0] && l[0][0] <= m+x)) return false;
+bool bfs(int mid, int x){
+    int visited[MAXSIZE+1][MAXSIZE+1] = {false};
+    queue <pair <int, int>> q;
 
-    bool visited[MAXSIZE+1][MAXSIZE+1] = {false};
-    queue<pair<int, int>> q;
+    if (!(x <= l[0][0] && l[0][0] <= mid+x)) return false;
 
     visited[0][0] = true;
-    q.push({0, 0});
+    q.push({0,0});
 
     while(!q.empty()){
         auto [ui, uj] = q.front(); q.pop();
+
         if(ui == n-1 && uj == n-1) return true;
 
         for(auto [di, dj] : dij){
             int vi = ui + di;
             int vj = uj + dj;
 
-            if (!isin(vi, vj) || visited[vi][vj]) continue;
-            if (m <= l[vi][vj] && l[vi][vj] <= m+x){
-                visited[vi][vj] = true;
-                q.push({vi,vj});
-            }
+            if(!(0 <= vi && vi < n && 0 <= vj && vj < n)) continue;
+            if(!(x <= l[vi][vj] && l[vi][vj] <= mid+x)) continue;
+            if(visited[vi][vj]) continue;
+
+            q.push({vi,vj});
+            visited[vi][vj] = true;
+
         }
     }
-
     return false;
-}
-
-int main() {
-    input();
-    solve();
 }
