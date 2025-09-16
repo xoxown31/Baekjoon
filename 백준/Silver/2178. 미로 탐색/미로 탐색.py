2803c6b7be9fe985f91c974fa2179da298bf5083
xoxown <73877449+xoxown31@users.noreply.github.com>
2025-09-16 18:45:47 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 114536 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,27 +1,43 @@
-import sys
 from collections import deque
+import sys
 
 input = sys.stdin.readline
 
+WALL = 0
+
+isin = lambda i,j : 0 <= i < N and 0 <= j < M
+iswall = lambda i,j : board[i][j] == WALL
+canmove = lambda i,j : isin(i,j) and not iswall(i,j)
+
+dij = [
+    (-1, 0),
+    ( 1, 0),
+    ( 0,-1),
+    ( 0, 1)
+]
+
 N, M = map(int, input().split())
 
-l = [[int(x) for x in input().strip()] for _ in range(N)]
+board = [[int(x) for x in input().strip()] for _ in range(N)]
 
 queue = deque()
-queue.append([0,0])
-dxy = [(0,1), (0,-1), (1,0), (-1,0)]
+visited = [[False] * M for _ in range(N)]
+
+queue.append((0, 0, 1))
+visited[0][0] = True
 
 while queue:
-	u = queue.popleft()
-	i, j = u
-	
-	for dx, dy in dxy:
-		x = i + dx
-		y = j + dy
-		
-		if x >= 0 and x < N and y >= 0 and y < M:
-			if l[x][y] > 0 and l[x][y] == 1:
-				l[x][y] = l[i][j] + 1
-				queue.append([x,y])
+    ui, uj, uw = queue.popleft()
 
-print(l[-1][-1])
+    if ui == N-1 and uj == M-1:
+        print(uw)
+        exit()
+
+    for di, dj in dij:
+
+        vi, vj, vw = ui+di, uj+dj, uw+1
+
+        if not canmove(vi,vj) or visited[vi][vj]: continue
+
+        queue.append((vi, vj, vw))
+        visited[vi][vj] = True
