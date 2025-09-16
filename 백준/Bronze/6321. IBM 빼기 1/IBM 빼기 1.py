4bde9b0785473afae8be83f3ea8040287f041e57
xoxown <73877449+xoxown31@users.noreply.github.com>
2025-09-16 18:33:01 +0900

    [Bronze III] Title: IBM 빼기 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6321. IBM 빼기 1/IBM 빼기 1.py
+++ 백준/Bronze/6321. IBM 빼기 1/IBM 빼기 1.py
@@ -1,5 +1,11 @@
-for i in range(int(input())):
-    t = ""
-    for j in input():
-        t += chr(ord(j)+1) if j != 'Z' else 'A'
-    print("String #{}".format(i+1) + '\n' + t + '\n')
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for test_case in range(T):
+
+    print("String #" + str(test_case+1))
+    print(''.join(chr(ord(c)+1) if ord(c) < ord('Z') else 'A' for c in input().strip()))
+    print()
