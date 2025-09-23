765c2c3fbf8a7fbfc8d2df43dda5f57a9e9ef0e4
xoxown <73877449+xoxown31@users.noreply.github.com>
2025-09-23 23:02:37 +0900

    [Gold IV] Title: 알파벳, Time: 2488 ms, Memory: 111276 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,38 +1,26 @@
 R, C = map(int, input().split())
+board = [input().strip() for _ in range(R)]
 
-board = [[x for x in input().strip()] for _ in range(R)]
-
+dij = [(0,1),(0,-1),(1,0),(-1,0)]
 isin = lambda i,j : 0 <= i < R and 0 <= j < C
 
-dij = [
-    ( 0,  1),
-    ( 0, -1),
-    ( 1,  0),
-    (-1,  0)
-]
+def f(x): return 1 << (ord(x) - 65)
 
-def f(x):
-    return 1 << (ord(x) - ord('A'))
-
-def g(a, b):
-    return a & b > 0
-
-res = -1
-
+res = 1
 stack = [(0, 0, 1, f(board[0][0]))]
 
 while stack:
-
     ui, uj, uw, us = stack.pop()
+    res = max(res, uw)
 
-    res = max(uw, res)
+    if res == 26:  # 가지치기
+        break
 
     for di, dj in dij:
+        vi, vj = ui + di, uj + dj
+        if not isin(vi, vj): continue
+        bit = f(board[vi][vj])
+        if us & bit: continue
+        stack.append((vi, vj, uw + 1, us | bit))
 
-        vi, vj, vw = ui + di, uj + dj, uw + 1
-        
-        if not isin(vi,vj) or g(us, f(board[vi][vj])): continue
-
-        stack.append((vi, vj, vw, us | f(board[vi][vj])))
-    
 print(res)
